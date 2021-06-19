df7e332afc658b8f3ec97884f55b8eb9b76256c5
Tuan Viet <lukasznguyen1999@gmail.com>
2021-06-19 11:53:17 +0200

    Created project in Spring Initializr

---
+++ src/main/java/com/example/jacketwholesaler/JacketWholesalerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jacketwholesaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacketWholesalerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JacketWholesalerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jacketwholesaler/JacketWholesalerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jacketwholesaler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacketWholesalerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
